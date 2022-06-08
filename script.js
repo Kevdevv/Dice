d634a2f535e825ccbaba8288574e54da19d15e8c
Kevin Rama <ramanantsoa.vlm@gmail.com>
2022-06-08 19:03:05 +0200

    ajout des fonction pour le 2e joueur, en cours : switch des joueur

--- script.js
+++ script.js
@@ -9,9 +9,46 @@ let currentPlayerOne = document.getElementById('current-p1')
 let currentPlayerTwo = document.getElementById('current-p2')
 let tab = []
 let scoreTab = []
+let tabTwo = []
+let scoreTabTwo = []
 let holdButton = document.getElementById('hold')
 
-function Dice(min, max) {
+
+
+let player = 1
+if (dice.innerHTML == '1') {
+    player = 2
+}
+console.log(dice.innerHTML)
+function whoPlay() {
+
+if (player == 1) {
+    DicePlayerOne()
+} else if (player == 2) {
+    DicePlayerTwo()
+}
+
+}
+
+
+function whoHold() {
+    let player = 1
+
+if (dice.innerHTML == '1') {
+    player = 2
+}
+
+if (player == 1) {
+    hold()
+} else if (player == 2) {
+    holdTwo()
+}
+}
+
+
+
+
+function DicePlayerOne(min, max) {
     
     min = 1
     max = 7
@@ -24,7 +61,15 @@ function Dice(min, max) {
         scoreTab = []
         currentPlayerOne.innerHTML = 0
         tab = []
+        //player = 2
     }
+/*
+    if (player == 2) {
+       rollDice.addEventListener('click', DicePlayerTwo)
+       //alert(player)
+    }
+    */
+    
 
     if (random > 1) {
         tab.push(random)
@@ -39,12 +84,41 @@ function Dice(min, max) {
     currentPlayerOne.innerHTML = sumWithInitial
 }
 
+function DicePlayerTwo(min, max) {
+    
+    min = 1
+    max = 7
+
+    let random = Math.floor(Math.random() * (max - min) + min)
+    dice.innerHTML = random
+    
+    if (random == 1) {
+        scorePlayerTwo.innerHTML = 0
+        scoreTabTwo = []
+        currentPlayerTwo.innerHTML = 0
+        tabTwo = []
+    }
+
+    if (random > 1) {
+        tabTwo.push(random)
+    }    
+
+    const initialValue = 0;
+    const sumWithInitial = tabTwo.reduce(
+      (previousValue, currentValue) => previousValue + currentValue,
+      initialValue
+    )
+    
+    currentPlayerTwo.innerHTML = sumWithInitial
+}
+
 
 function reload() {
     location.reload()
 }
 
 function hold() {
+    alert('one')
     scorePlayerOne.innerHTML = currentPlayerOne.innerHTML
    
     let score = scorePlayerOne.innerHTML
@@ -63,6 +137,28 @@ function hold() {
     tab = []
 }
 
-rollDice.addEventListener('click', Dice)
+function holdTwo() {
+    alert('two')
+    scorePlayerTwo.innerHTML = currentPlayerTwo.innerHTML
+   
+    let score = scorePlayerTwo.innerHTML
+    
+    scoreTabTwo.push(score)
+
+    const initialValue = 0;
+    const sumWithInitial = scoreTabTwo.reduce(
+      (previousValue, currentValue) => parseInt(previousValue) + parseInt(currentValue),
+      initialValue
+    )
+    
+    scorePlayerTwo.innerHTML = sumWithInitial
+
+    currentPlayerTwo.innerHTML = 0
+    tabTwo = []
+}
+
+
+rollDice.addEventListener('click', whoPlay)
+//rollDice.addEventListener('click', DicePlayerTwo)
 newGame.addEventListener('click', reload)
-holdButton.addEventListener('click', hold)
+holdButton.addEventListener('click', whoHold)
